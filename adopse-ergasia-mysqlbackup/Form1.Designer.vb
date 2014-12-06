81a53be5a9fc255970ff18142a43565bd4119425
kselini <kselini@gmail.com>
2014-12-06 11:39:20 +0000

    User InterFace version 1.0

--- adopse-ergasia-mysqlbackup/Form1.Designer.vb
+++ adopse-ergasia-mysqlbackup/Form1.Designer.vb
@@ -22,24 +22,23 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Button2 = New System.Windows.Forms.Button
         Me.ToolStrip1 = New System.Windows.Forms.ToolStrip
-        Me.ToolStrip2 = New System.Windows.Forms.ToolStrip
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox
-        Me.GroupBox2 = New System.Windows.Forms.GroupBox
-        Me.GroupBox3 = New System.Windows.Forms.GroupBox
-        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox
-        Me.ButtonConnectToMySQLServer = New System.Windows.Forms.Button
         Me.ToolStripLabelFile = New System.Windows.Forms.ToolStripLabel
         Me.ToolStripLabelTools = New System.Windows.Forms.ToolStripLabel
         Me.ToolStripLabelHelp = New System.Windows.Forms.ToolStripLabel
+        Me.ToolStrip2 = New System.Windows.Forms.ToolStrip
+        Me.ToolStripButtonNew = New System.Windows.Forms.ToolStripButton
+        Me.ToolStripButtonOpen = New System.Windows.Forms.ToolStripButton
         Me.ToolStripButtonSave = New System.Windows.Forms.ToolStripButton
         Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator
-        Me.ToolStripButtonOpen = New System.Windows.Forms.ToolStripButton
-        Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton
-        Me.ToolStripButtonLog = New System.Windows.Forms.ToolStripButton
         Me.ToolStripButtonJobs = New System.Windows.Forms.ToolStripButton
+        Me.ToolStripButtonLog = New System.Windows.Forms.ToolStripButton
+        Me.GroupBoxStrorage = New System.Windows.Forms.GroupBox
+        Me.GroupBoxEmail = New System.Windows.Forms.GroupBox
+        Me.GroupBoxJobs = New System.Windows.Forms.GroupBox
+        Me.CheckedListBoxDatabases = New System.Windows.Forms.CheckedListBox
+        Me.ButtonConnectToMySQLServer = New System.Windows.Forms.Button
         Me.ToolStrip1.SuspendLayout()
         Me.ToolStrip2.SuspendLayout()
         Me.SuspendLayout()
@@ -58,62 +57,9 @@ Partial Class Form1
         Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripLabelFile, Me.ToolStripLabelTools, Me.ToolStripLabelHelp})
         Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
         Me.ToolStrip1.Name = "ToolStrip1"
-        Me.ToolStrip1.Size = New System.Drawing.Size(506, 25)
+        Me.ToolStrip1.Size = New System.Drawing.Size(522, 25)
         Me.ToolStrip1.TabIndex = 2
-        Me.ToolStrip1.Text = "ToolStrip1"
-        '
-        'ToolStrip2
-        '
-        Me.ToolStrip2.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton2, Me.ToolStripButtonOpen, Me.ToolStripButtonSave, Me.ToolStripSeparator1, Me.ToolStripButtonJobs, Me.ToolStripButtonLog})
-        Me.ToolStrip2.Location = New System.Drawing.Point(0, 25)
-        Me.ToolStrip2.Name = "ToolStrip2"
-        Me.ToolStrip2.Size = New System.Drawing.Size(506, 38)
-        Me.ToolStrip2.TabIndex = 3
-        Me.ToolStrip2.Text = "ToolStrip2"
-        '
-        'GroupBox1
-        '
-        Me.GroupBox1.Location = New System.Drawing.Point(261, 66)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(233, 71)
-        Me.GroupBox1.TabIndex = 4
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "Storage"
-        '
-        'GroupBox2
-        '
-        Me.GroupBox2.Location = New System.Drawing.Point(261, 164)
-        Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(233, 75)
-        Me.GroupBox2.TabIndex = 5
-        Me.GroupBox2.TabStop = False
-        Me.GroupBox2.Text = "Email"
-        '
-        'GroupBox3
-        '
-        Me.GroupBox3.Location = New System.Drawing.Point(261, 267)
-        Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(233, 83)
-        Me.GroupBox3.TabIndex = 6
-        Me.GroupBox3.TabStop = False
-        Me.GroupBox3.Text = "Jobs"
-        '
-        'CheckedListBox1
-        '
-        Me.CheckedListBox1.FormattingEnabled = True
-        Me.CheckedListBox1.Location = New System.Drawing.Point(12, 126)
-        Me.CheckedListBox1.Name = "CheckedListBox1"
-        Me.CheckedListBox1.Size = New System.Drawing.Size(194, 244)
-        Me.CheckedListBox1.TabIndex = 7
-        '
-        'ButtonConnectToMySQLServer
-        '
-        Me.ButtonConnectToMySQLServer.Location = New System.Drawing.Point(12, 79)
-        Me.ButtonConnectToMySQLServer.Name = "ButtonConnectToMySQLServer"
-        Me.ButtonConnectToMySQLServer.Size = New System.Drawing.Size(116, 23)
-        Me.ButtonConnectToMySQLServer.TabIndex = 8
-        Me.ButtonConnectToMySQLServer.Text = "Connect to MySQL Server"
-        Me.ButtonConnectToMySQLServer.UseVisualStyleBackColor = True
+        Me.ToolStrip1.Text = "ToolStripSimple"
         '
         'ToolStripLabelFile
         '
@@ -133,9 +79,39 @@ Partial Class Form1
         Me.ToolStripLabelHelp.Size = New System.Drawing.Size(32, 22)
         Me.ToolStripLabelHelp.Text = "Help"
         '
+        'ToolStrip2
+        '
+        Me.ToolStrip2.ImageScalingSize = New System.Drawing.Size(50, 50)
+        Me.ToolStrip2.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButtonNew, Me.ToolStripButtonOpen, Me.ToolStripButtonSave, Me.ToolStripSeparator1, Me.ToolStripButtonJobs, Me.ToolStripButtonLog})
+        Me.ToolStrip2.Location = New System.Drawing.Point(0, 25)
+        Me.ToolStrip2.Name = "ToolStrip2"
+        Me.ToolStrip2.Size = New System.Drawing.Size(522, 38)
+        Me.ToolStrip2.TabIndex = 3
+        Me.ToolStrip2.Text = "ToolStripIcons"
+        '
+        'ToolStripButtonNew
+        '
+        Me.ToolStripButtonNew.Image = Global.adopse_ergasia_mysqlbackup.My.Resources.Resources.download__3_
+        Me.ToolStripButtonNew.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButtonNew.Name = "ToolStripButtonNew"
+        Me.ToolStripButtonNew.Size = New System.Drawing.Size(35, 35)
+        Me.ToolStripButtonNew.Text = "New"
+        Me.ToolStripButtonNew.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.ToolStripButtonNew.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'ToolStripButtonOpen
+        '
+        Me.ToolStripButtonOpen.Image = Global.adopse_ergasia_mysqlbackup.My.Resources.Resources.images__1_
+        Me.ToolStripButtonOpen.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButtonOpen.Name = "ToolStripButtonOpen"
+        Me.ToolStripButtonOpen.Size = New System.Drawing.Size(40, 35)
+        Me.ToolStripButtonOpen.Text = "Open"
+        Me.ToolStripButtonOpen.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.ToolStripButtonOpen.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
         'ToolStripButtonSave
         '
-        Me.ToolStripButtonSave.Image = CType(resources.GetObject("ToolStripButtonSave.Image"), System.Drawing.Image)
+        Me.ToolStripButtonSave.Image = Global.adopse_ergasia_mysqlbackup.My.Resources.Resources.download__2_
         Me.ToolStripButtonSave.ImageTransparentColor = System.Drawing.Color.Magenta
         Me.ToolStripButtonSave.Name = "ToolStripButtonSave"
         Me.ToolStripButtonSave.Size = New System.Drawing.Size(35, 35)
@@ -148,39 +124,9 @@ Partial Class Form1
         Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
         Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 38)
         '
-        'ToolStripButtonOpen
-        '
-        Me.ToolStripButtonOpen.Image = CType(resources.GetObject("ToolStripButtonOpen.Image"), System.Drawing.Image)
-        Me.ToolStripButtonOpen.ImageTransparentColor = System.Drawing.Color.Magenta
-        Me.ToolStripButtonOpen.Name = "ToolStripButtonOpen"
-        Me.ToolStripButtonOpen.Size = New System.Drawing.Size(40, 35)
-        Me.ToolStripButtonOpen.Text = "Open"
-        Me.ToolStripButtonOpen.TextAlign = System.Drawing.ContentAlignment.BottomCenter
-        Me.ToolStripButtonOpen.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
-        '
-        'ToolStripButton2
-        '
-        Me.ToolStripButton2.Image = CType(resources.GetObject("ToolStripButton2.Image"), System.Drawing.Image)
-        Me.ToolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta
-        Me.ToolStripButton2.Name = "ToolStripButton2"
-        Me.ToolStripButton2.Size = New System.Drawing.Size(35, 35)
-        Me.ToolStripButton2.Text = "New"
-        Me.ToolStripButton2.TextAlign = System.Drawing.ContentAlignment.BottomCenter
-        Me.ToolStripButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
-        '
-        'ToolStripButtonLog
-        '
-        Me.ToolStripButtonLog.Image = CType(resources.GetObject("ToolStripButtonLog.Image"), System.Drawing.Image)
-        Me.ToolStripButtonLog.ImageTransparentColor = System.Drawing.Color.Magenta
-        Me.ToolStripButtonLog.Name = "ToolStripButtonLog"
-        Me.ToolStripButtonLog.Size = New System.Drawing.Size(31, 35)
-        Me.ToolStripButtonLog.Text = "Log"
-        Me.ToolStripButtonLog.TextAlign = System.Drawing.ContentAlignment.BottomCenter
-        Me.ToolStripButtonLog.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
-        '
         'ToolStripButtonJobs
         '
-        Me.ToolStripButtonJobs.Image = CType(resources.GetObject("ToolStripButtonJobs.Image"), System.Drawing.Image)
+        Me.ToolStripButtonJobs.Image = Global.adopse_ergasia_mysqlbackup.My.Resources.Resources.download__6_
         Me.ToolStripButtonJobs.ImageTransparentColor = System.Drawing.Color.Magenta
         Me.ToolStripButtonJobs.Name = "ToolStripButtonJobs"
         Me.ToolStripButtonJobs.Size = New System.Drawing.Size(34, 35)
@@ -188,16 +134,70 @@ Partial Class Form1
         Me.ToolStripButtonJobs.TextAlign = System.Drawing.ContentAlignment.BottomCenter
         Me.ToolStripButtonJobs.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
         '
+        'ToolStripButtonLog
+        '
+        Me.ToolStripButtonLog.Image = Global.adopse_ergasia_mysqlbackup.My.Resources.Resources.download__5_
+        Me.ToolStripButtonLog.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButtonLog.Name = "ToolStripButtonLog"
+        Me.ToolStripButtonLog.Size = New System.Drawing.Size(31, 35)
+        Me.ToolStripButtonLog.Text = "Log"
+        Me.ToolStripButtonLog.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.ToolStripButtonLog.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'GroupBoxStrorage
+        '
+        Me.GroupBoxStrorage.Location = New System.Drawing.Point(261, 94)
+        Me.GroupBoxStrorage.Name = "GroupBoxStrorage"
+        Me.GroupBoxStrorage.Size = New System.Drawing.Size(233, 71)
+        Me.GroupBoxStrorage.TabIndex = 4
+        Me.GroupBoxStrorage.TabStop = False
+        Me.GroupBoxStrorage.Text = "Storage"
+        '
+        'GroupBoxEmail
+        '
+        Me.GroupBoxEmail.Location = New System.Drawing.Point(261, 198)
+        Me.GroupBoxEmail.Name = "GroupBoxEmail"
+        Me.GroupBoxEmail.Size = New System.Drawing.Size(233, 63)
+        Me.GroupBoxEmail.TabIndex = 5
+        Me.GroupBoxEmail.TabStop = False
+        Me.GroupBoxEmail.Text = "Email"
+        '
+        'GroupBoxJobs
+        '
+        Me.GroupBoxJobs.Location = New System.Drawing.Point(261, 290)
+        Me.GroupBoxJobs.Name = "GroupBoxJobs"
+        Me.GroupBoxJobs.Size = New System.Drawing.Size(233, 55)
+        Me.GroupBoxJobs.TabIndex = 6
+        Me.GroupBoxJobs.TabStop = False
+        Me.GroupBoxJobs.Text = "Jobs"
+        '
+        'CheckedListBoxDatabases
+        '
+        Me.CheckedListBoxDatabases.FormattingEnabled = True
+        Me.CheckedListBoxDatabases.Location = New System.Drawing.Point(12, 126)
+        Me.CheckedListBoxDatabases.Name = "CheckedListBoxDatabases"
+        Me.CheckedListBoxDatabases.Size = New System.Drawing.Size(194, 244)
+        Me.CheckedListBoxDatabases.TabIndex = 7
+        '
+        'ButtonConnectToMySQLServer
+        '
+        Me.ButtonConnectToMySQLServer.Location = New System.Drawing.Point(12, 85)
+        Me.ButtonConnectToMySQLServer.Name = "ButtonConnectToMySQLServer"
+        Me.ButtonConnectToMySQLServer.Size = New System.Drawing.Size(127, 23)
+        Me.ButtonConnectToMySQLServer.TabIndex = 8
+        Me.ButtonConnectToMySQLServer.Text = "Connect to MySQL Server"
+        Me.ButtonConnectToMySQLServer.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(506, 394)
+        Me.ClientSize = New System.Drawing.Size(522, 394)
         Me.Controls.Add(Me.ButtonConnectToMySQLServer)
-        Me.Controls.Add(Me.CheckedListBox1)
-        Me.Controls.Add(Me.GroupBox3)
-        Me.Controls.Add(Me.GroupBox2)
-        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.CheckedListBoxDatabases)
+        Me.Controls.Add(Me.GroupBoxJobs)
+        Me.Controls.Add(Me.GroupBoxEmail)
+        Me.Controls.Add(Me.GroupBoxStrorage)
         Me.Controls.Add(Me.ToolStrip2)
         Me.Controls.Add(Me.ToolStrip1)
         Me.Controls.Add(Me.Button2)
@@ -214,17 +214,17 @@ Partial Class Form1
     Friend WithEvents Button2 As System.Windows.Forms.Button
     Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
     Friend WithEvents ToolStrip2 As System.Windows.Forms.ToolStrip
-    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
-    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
-    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
-    Friend WithEvents CheckedListBox1 As System.Windows.Forms.CheckedListBox
+    Friend WithEvents GroupBoxStrorage As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBoxEmail As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBoxJobs As System.Windows.Forms.GroupBox
+    Friend WithEvents CheckedListBoxDatabases As System.Windows.Forms.CheckedListBox
     Friend WithEvents ButtonConnectToMySQLServer As System.Windows.Forms.Button
     Friend WithEvents ToolStripLabelFile As System.Windows.Forms.ToolStripLabel
     Friend WithEvents ToolStripLabelTools As System.Windows.Forms.ToolStripLabel
     Friend WithEvents ToolStripLabelHelp As System.Windows.Forms.ToolStripLabel
     Friend WithEvents ToolStripButtonSave As System.Windows.Forms.ToolStripButton
     Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
-    Friend WithEvents ToolStripButton2 As System.Windows.Forms.ToolStripButton
+    Friend WithEvents ToolStripButtonNew As System.Windows.Forms.ToolStripButton
     Friend WithEvents ToolStripButtonOpen As System.Windows.Forms.ToolStripButton
     Friend WithEvents ToolStripButtonJobs As System.Windows.Forms.ToolStripButton
     Friend WithEvents ToolStripButtonLog As System.Windows.Forms.ToolStripButton
