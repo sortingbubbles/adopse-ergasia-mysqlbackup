fd33d2f4181543b7367536a2a18bca3b5d8fabdd
tazzyannie <tazzyannie@gmail.com>
2014-12-18 03:07:53 +0000

    Υλοποίηση αυθεντικοποίησης χρήστη για το onedrive και υποδοχή του access token

---
+++ OneDriveLogin/OneDriveLogin/Form1.vb
@@ -0,0 +1,49 @@
+﻿Imports System
+Imports System.IO
+Imports System.Net
+Imports System.Text
+Imports Microsoft.Live
+
+Public Class Form1
+
+
+    Private Const scope As String = "wl.skydrive_update" ' scope για upload/download
+    Private Const cliend_id As String = "0000000040139D3E" ' cliend id της εφαρμογής
+
+    'Διεύθυνση για αυθεντικοποίηση και request για token
+    Public Const address As String = "https://login.live.com/oauth20_authorize.srf?client_id=0000000040139D3E&scope=wl.skydrive_update&response_type=token&redirect_uri=https://login.live.com/oauth20_desktop.srf"
+
+
+   
+    'Dim request As HttpWebRequest
+    'Dim response As HttpWebResponse = Nothing
+    'Dim reader As StreamReader
+
+    'Είσοδος στο one drive για αυθεντικοποίηση και token
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            ' Καλεί την 2η φόρμα που είναι ο web browser 
+            webBrowser.Show()
+            ' request = DirectCast(WebRequest.Create(address), HttpWebRequest)
+            '  response = DirectCast(request.GetResponse(), HttpWebResponse)
+            '  reader = New StreamReader(response.GetResponseStream())
+            ' Console.WriteLine(reader.ReadToEnd())
+        Catch ex As Exception
+            Console.WriteLine("You fool!")
+        Finally
+            'If Not response Is Nothing Then response.Close()
+        End Try
+    End Sub
+
+
+    'Ανέβασμα αρχείων
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        webBrowser.Show()
+        webBrowser.uploadFile()
+    End Sub
+
+
+
+
+   
+End Class
---
+++ OneDriveLogin/OneDriveLogin/webBrowser.vb
@@ -0,0 +1,33 @@
+﻿Public Class webBrowser
+
+
+    Public token As String = ""
+
+    'Μετά το φόρτωμα της σελίδας ελέγχεται εάν το url έχει το access token
+    'Εάν υπάρχει το αποθηκεύει στη μεταβλητή token
+    Private Sub WebBrowser1_Navigated(sender As Object, e As WebBrowserNavigatedEventArgs) Handles WebBrowser1.Navigated
+        Dim uriString() As String
+        If (WebBrowser1.Url.ToString.Contains("#access_token")) Then
+            uriString = WebBrowser1.Url.Fragment.Split("=&".ToCharArray)
+            token = uriString(1)
+            Console.WriteLine(token)
+            WebBrowser1.Visible = False
+        End If
+
+
+        ' για debugging του upload
+        Console.WriteLine(WebBrowser1.Url.ToString())
+        Console.WriteLine(WebBrowser1.DocumentText)
+
+    End Sub
+
+
+    ' Ανέβασμα αρχείων, καλείται από την Form1 μόλις πατηθεί το 2ο κουμπί 
+    ' Δεν έχω καταλάβει ακόμη που πρέπει να βρίσκεται το αρχείο που θέλουμε να ανεβάσουμε... 
+    Public Sub uploadFile()
+        Dim fileToUpload As String = "fuckthis.txt"
+        Dim uploadAddress As String = "https://apis.live.net/v5.0/me/skydrive/files/" + fileToUpload + "?access_token=" + token
+
+        WebBrowser1.Navigate(uploadAddress)
+    End Sub
+End Class
