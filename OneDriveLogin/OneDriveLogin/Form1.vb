2869a9c3d2aca28d2aded5ccaf989c64e07d2404
tazzyannie <tazzyannie@gmail.com>
2014-12-24 00:42:25 +0000

    Αυθεντικοποίηση μέσω webBrowser, access token & refresh token από json response στη κονσόλα και λειτουργικό upload αρχείων στο onedrive

--- OneDriveLogin/OneDriveLogin/Form1.vb
+++ OneDriveLogin/OneDriveLogin/Form1.vb
@@ -5,45 +5,37 @@ Imports System.Text
 Imports Microsoft.Live
 
 Public Class Form1
-
-
     Private Const scope As String = "wl.skydrive_update" ' scope για upload/download
-    Private Const cliend_id As String = "0000000040139D3E" ' cliend id της εφαρμογής
+    Public Const cliend_id As String = "0000000040139D3E" ' cliend id της εφαρμογής
 
     'Διεύθυνση για αυθεντικοποίηση και request για token
-    Public Const address As String = "https://login.live.com/oauth20_authorize.srf?client_id=0000000040139D3E&scope=wl.skydrive_update&response_type=token&redirect_uri=https://login.live.com/oauth20_desktop.srf"
+    Public Const address As String = "https://login.live.com/oauth20_authorize.srf?client_id=0000000040139D3E&scope=wl.skydrive_update%20wl.offline_access&response_type=code&redirect_uri=https://login.live.com/oauth20_desktop.srf"
 
-
-   
-    'Dim request As HttpWebRequest
-    'Dim response As HttpWebResponse = Nothing
-    'Dim reader As StreamReader
 
     'Είσοδος στο one drive για αυθεντικοποίηση και token
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Try
-            ' Καλεί την 2η φόρμα που είναι ο web browser 
-            webBrowser.Show()
-            ' request = DirectCast(WebRequest.Create(address), HttpWebRequest)
-            '  response = DirectCast(request.GetResponse(), HttpWebResponse)
-            '  reader = New StreamReader(response.GetResponseStream())
-            ' Console.WriteLine(reader.ReadToEnd())
-        Catch ex As Exception
-            Console.WriteLine("You fool!")
-        Finally
-            'If Not response Is Nothing Then response.Close()
-        End Try
+        webBrowser.Show()
     End Sub
 
+   
 
     'Ανέβασμα αρχείων
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        webBrowser.Show()
-        webBrowser.uploadFile()
+        Dim fileName As String = String.Empty
+        Using dlg = New OpenFileDialog()
+            dlg.Filter = "All Files|*.*"
+            If dlg.ShowDialog(Me) = DialogResult.OK Then
+                fileName = dlg.FileName
+            End If
+        End Using
+        Dim url As String = String.Format("https://apis.live.net/v5.0/me/skydrive/Files/{0}?access_token={1}", Path.GetFileName(fileName), webBrowser.token)
+        Using client = New WebClient()
+            client.UploadDataAsync(New Uri(url), "PUT", File.ReadAllBytes(fileName))
+        End Using
     End Sub
 
 
 
 
-   
+
 End Class
--- OneDriveLogin/OneDriveLogin/webBrowser.vb
+++ OneDriveLogin/OneDriveLogin/webBrowser.vb
@@ -1,33 +1,63 @@
-﻿Public Class webBrowser
+﻿Imports System.Net
+Imports System.IO
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+Public Class webBrowser
 
 
-    Public token As String = ""
+    Public token As String = "EwBwAq1DBAAUGCCXc8wU/zFu9QnLdZXy+YnElFkAAbty5U6w4KguEyqJx4HcGJsMUKd/24zRHR4fI7b7lOAXHDPFh1i8JCxLFvI9Y7M2wsAROBduEtz2L1dAKtx6/OX2ifWlTHXcqS5SR7QRKjRbfk+TXQvhNGC4u5DfverP/Wzo9Kis+kaibmyzrff8lyLM57MmZkANEZIae/TbiXY5Cx6ha+whi9gIQFPRCBQIiX/U0v+VAUHrKleZ6VsF4Vr9xsByl6g1n1cYz3cUBN/MBLNVCJ6/BchkP01G0gpxc2IHsoY7LrkDCd4jrDVL1mKYaTB5uMsmSznNf3ZRIqOtr++EoTICL0i0WzJ2GHefZNfO/406jkF4FyJWmneKwDMDZgAACF1cchVch34OQAHhy0Y9ZeCwH4Z3ZLv59aXot18tm6DO8B9XkUpu9vxGfM3eYLK1MXS90FN9kiM/AQLpQlVDY3311nXKv6AI4Q89ozdfr4ikQQJikpjzii9tEi5GwppEDAJiSwl0Imv/ufWALCw3d6ld4CtXnf+EBcJnPOwyVLDGtYY3re9NpTxUY0PnyAWu2mZ9hSkKMEEWeV/2ndSfvUGXf35CFaPaBd0POPm0/DKjtOLITVlwOEj7lC1r2fWYd7t9epUNUpyFt3WztCMmupJByqtG3n26+F4tenSM2Cs3MSjLw/aokVbzoYrSnl13fUingxBNyBOq9bgkFho9iQHbeKOc+igvsNZ6thKbEbKiZQQsiPB22X4J9nNwiOCJQTOxiS7XRZhAaHPEkYs5nTaHxQslAg56K4lY2Wd7AknPPFK4PF1kz0hM/1YB"
+    Public code As String = ""
+
 
     'Μετά το φόρτωμα της σελίδας ελέγχεται εάν το url έχει το access token
     'Εάν υπάρχει το αποθηκεύει στη μεταβλητή token
     Private Sub WebBrowser1_Navigated(sender As Object, e As WebBrowserNavigatedEventArgs) Handles WebBrowser1.Navigated
         Dim uriString() As String
-        If (WebBrowser1.Url.ToString.Contains("#access_token")) Then
-            uriString = WebBrowser1.Url.Fragment.Split("=&".ToCharArray)
-            token = uriString(1)
+        If (WebBrowser1.Url.ToString.Contains("?code")) Then
+            uriString = WebBrowser1.Url.ToString.Split("=&".ToCharArray)
+            code = uriString(1)
             Console.WriteLine(token)
             WebBrowser1.Visible = False
+            GetTokens()
+
+
+
         End If
 
 
         ' για debugging του upload
         Console.WriteLine(WebBrowser1.Url.ToString())
-        Console.WriteLine(WebBrowser1.DocumentText)
+
+
+    End Sub
+
+    Private Sub GetTokens()
+
+
+
+        Dim url As String = String.Format("https://login.live.com/oauth20_token.srf?client_id={0}&client_secret=qkMMeEhpLbM5GAfVJRWVPfo9PzuIjEGD&code={1}&grant_type=authorization_code&redirect_uri=https://login.live.com/oauth20_desktop.srf", Form1.cliend_id, code)
+
+        Dim req As HttpWebRequest
+        req = HttpWebRequest.Create(url)
+
+        Dim objStream As Stream
+
+        objStream = req.GetResponse().GetResponseStream()
+
+        Dim objReader As New StreamReader(objStream)
+
+        Dim json As String = objReader.ReadLine()
+        Dim ser As JObject = JObject.Parse(json)
+        Dim data As List(Of JToken) = ser.Children().ToList
+        For Each item As JProperty In data
+            Console.WriteLine(item.Value)
+
+        Next
+
+
+
 
     End Sub
 
 
-    ' Ανέβασμα αρχείων, καλείται από την Form1 μόλις πατηθεί το 2ο κουμπί 
-    ' Δεν έχω καταλάβει ακόμη που πρέπει να βρίσκεται το αρχείο που θέλουμε να ανεβάσουμε... 
-    Public Sub uploadFile()
-        Dim fileToUpload As String = "fuckthis.txt"
-        Dim uploadAddress As String = "https://apis.live.net/v5.0/me/skydrive/files/" + fileToUpload + "?access_token=" + token
-
-        WebBrowser1.Navigate(uploadAddress)
-    End Sub
 End Class
