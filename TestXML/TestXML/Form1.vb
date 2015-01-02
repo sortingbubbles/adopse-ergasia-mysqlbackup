7fe3b3ead531fa8ce25bf88223d14cea5d494cfe
gcharitak <gcharitak@gmail.com>
2015-01-02 19:00:26 +0000

    Ato o project dimiourgei ena xml arxeio sto fakelo Documents tou xristi

---
+++ TestXML/TestXML/Form1.vb
@@ -0,0 +1,53 @@
+﻿Imports System.Xml
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        xmlexample()
+    End Sub
+
+    Private Sub xmlexample()
+        Dim xmlDoc As XmlDocument = New XmlDocument()
+
+        ' Write down the XML declaration
+        Dim xmlDeclaration As XmlDeclaration = xmlDoc.CreateXmlDeclaration("1.0", "utf-8", Nothing)
+
+        ' Create the root element
+        Dim rootNode As XmlElement = xmlDoc.CreateElement("CategoryList")
+        xmlDoc.InsertBefore(xmlDeclaration, xmlDoc.DocumentElement)
+        xmlDoc.AppendChild(rootNode)
+
+        ' Create a new <Category> element and add it to the root node
+        Dim parentNode As XmlElement = xmlDoc.CreateElement("Category")
+
+        ' Set attribute name and value!
+        parentNode.SetAttribute("ID", "01")
+
+        xmlDoc.DocumentElement.PrependChild(parentNode)
+
+        ' Create the required nodes
+        Dim mainNode As XmlElement = xmlDoc.CreateElement("MainCategory")
+        Dim descNode As XmlElement = xmlDoc.CreateElement("Description")
+        Dim activeNode As XmlElement = xmlDoc.CreateElement("Active")
+
+        ' retrieve the text 
+        Dim categoryText As XmlText = xmlDoc.CreateTextNode("XML")
+        Dim descText As XmlText = xmlDoc.CreateTextNode("This is a list my XML articles.")
+        Dim activeText As XmlText = xmlDoc.CreateTextNode("true")
+
+        'append the nodes to the parentNode without the value
+        parentNode.AppendChild(mainNode)
+        parentNode.AppendChild(descNode)
+        parentNode.AppendChild(activeNode)
+
+        'save the value of the fields into the nodes
+        mainNode.AppendChild(categoryText)
+        descNode.AppendChild(descText)
+        activeNode.AppendChild(activeText)
+
+        'Save to the XML file
+        FileIO.SpecialDirectories.MyDocuments.ToString()
+        xmlDoc.Save(FileIO.SpecialDirectories.MyDocuments.ToString() & "\categories.xml")
+    End Sub
+
+End Class
---
+++ TestXML/TestXML/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("TestXML")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("TestXML")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("712937ee-63df-4c63-821c-080914d2b9f5")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
