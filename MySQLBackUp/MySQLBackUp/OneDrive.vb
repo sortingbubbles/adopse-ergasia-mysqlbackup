b56fcfc219b54f6219a74edf01943ae5d3a2cf5e
tazzyannie <tazzyannie@gmail.com>
2015-01-08 20:20:08 +0000

    Υλοποίηση της μεθόδου Save στην κλάση OneDrive για αποθήκευση σε xml αρχείο.

--- MySQLBackUp/MySQLBackUp/OneDrive.vb
+++ MySQLBackUp/MySQLBackUp/OneDrive.vb
@@ -6,9 +6,6 @@ Public Class OneDrive
     Private clientSecret As String = "*************************" ' client secret της εφαρμογής
     Private oneDriveBrowser As OneDriveBrowser = New OneDriveBrowser(clientID, clientSecret)
 
-    Private authorizationCode As String
-    Private refreshToken As String
-
 
     Public Overrides Sub Authenticate()
         Dim url As String = String.Format("https://login.live.com/oauth20_authorize.srf?client_id={0}&scope=wl.skydrive_update%20wl.offline_access&response_type=code&redirect_uri=https://login.live.com/oauth20_desktop.srf", clientID)
@@ -17,7 +14,28 @@ Public Class OneDrive
     End Sub
 
     Public Overrides Sub Save(XmlDocPath As String)
+        Dim myXmlDocument As XmlDocument = New XmlDocument()
+        myXmlDocument.Load(XmlDocPath)
+        Dim root As XmlNode = myXmlDocument.DocumentElement
+        For Each node As XmlNode In root.ChildNodes
+            If node.Name = "tasks" Then
+                Dim taskNode As XmlElement = myXmlDocument.CreateElement("task")
+                taskNode.SetAttribute("app_id", "onedrive")
+                node.AppendChild(taskNode)
 
+                Dim authCodePathNode As XmlElement = myXmlDocument.CreateElement("authCodePath")
+                Dim refreshTokenPathNode As XmlElement = myXmlDocument.CreateElement("refreshTokenPath")
+                Dim authCodePathText As XmlText = myXmlDocument.CreateTextNode("C:\TEMP\authCode.txt")
+                Dim refreshTokenPathText As XmlText = myXmlDocument.CreateTextNode("C:\TEMP\OneDriveRefreshToken.txt")
+
+                taskNode.AppendChild(authCodePathNode)
+                taskNode.AppendChild(refreshTokenPathNode)
+                authCodePathNode.AppendChild(authCodePathText)
+                refreshTokenPathNode.AppendChild(refreshTokenPathText)
+            End If
+        Next
+
+        myXmlDocument.Save(XmlDocPath)
     End Sub
 
 
