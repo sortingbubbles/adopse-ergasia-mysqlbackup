3effe62d744e9191da458466ac76628dc5233b9d
tazzyannie <tazzyannie@gmail.com>
2015-01-09 13:29:16 +0000

    Bελτιστοποίηση της μεθόδου Save στη κλάση OneDrive.

--- MySQLBackUp/MySQLBackUp/OneDrive.vb
+++ MySQLBackUp/MySQLBackUp/OneDrive.vb
@@ -4,39 +4,37 @@ Public Class OneDrive
     Inherits CloudService
     Private clientID As String = "*********************" ' client id της εφαρμογής
     Private clientSecret As String = "*************************" ' client secret της εφαρμογής
+    'Φυλλομετρητής που εμφανίζεται για την ταυτοποίηση του χρήστη
     Private oneDriveBrowser As OneDriveBrowser = New OneDriveBrowser(clientID, clientSecret)
 
-
+    'Μέθοδος για την αυθευντικοποίηση του χρήστη στο OneDrive
+    'Κληρονομείται από τη baseclass CloudService
     Public Overrides Sub Authenticate()
+        'Προετοιμασία url για αίτηση αυθεντικοποίησης
         Dim url As String = String.Format("https://login.live.com/oauth20_authorize.srf?client_id={0}&scope=wl.skydrive_update%20wl.offline_access&response_type=code&redirect_uri=https://login.live.com/oauth20_desktop.srf", clientID)
         oneDriveBrowser.OneDriveAuth.Url = New Uri(url)
+        'Εμφάνιση του browser για την εισαγωγή των στοιχείων του χρήστη
         oneDriveBrowser.Show()
     End Sub
 
-    Public Overrides Sub Save(XmlDocPath As String)
-        Dim myXmlDocument As XmlDocument = New XmlDocument()
-        myXmlDocument.Load(XmlDocPath)
-        Dim root As XmlNode = myXmlDocument.DocumentElement
-        For Each node As XmlNode In root.ChildNodes
-            If node.Name = "tasks" Then
-                Dim taskNode As XmlElement = myXmlDocument.CreateElement("task")
-                taskNode.SetAttribute("app_id", "onedrive")
-                node.AppendChild(taskNode)
 
-                Dim authCodePathNode As XmlElement = myXmlDocument.CreateElement("authCodePath")
-                Dim refreshTokenPathNode As XmlElement = myXmlDocument.CreateElement("refreshTokenPath")
-                Dim authCodePathText As XmlText = myXmlDocument.CreateTextNode("C:\TEMP\authCode.txt")
-                Dim refreshTokenPathText As XmlText = myXmlDocument.CreateTextNode("C:\TEMP\OneDriveRefreshToken.txt")
+    'Μέθοδος για την αποθήκευση των απαραίτητων πεδίων για το upload στο OneDrive
+    'Κληρονομείται από τη baseclass CloudService
+    Public Overrides Sub Save(ByRef XmlDoc As XmlDocument)
+        Dim tasksNode As XmlNode = XmlDoc.GetElementsByTagName("tasks").Item(0)
+        Dim taskNode As XmlElement = XmlDoc.CreateElement("task")
+        taskNode.SetAttribute("app_id", "onedrive")
+        tasksNode.AppendChild(taskNode)
 
-                taskNode.AppendChild(authCodePathNode)
-                taskNode.AppendChild(refreshTokenPathNode)
-                authCodePathNode.AppendChild(authCodePathText)
-                refreshTokenPathNode.AppendChild(refreshTokenPathText)
-            End If
-        Next
+        Dim authCodePathNode As XmlElement = XmlDoc.CreateElement("authCodePath")
+        Dim refreshTokenPathNode As XmlElement = XmlDoc.CreateElement("refreshTokenPath")
+        Dim authCodePathText As XmlText = XmlDoc.CreateTextNode("C:\TEMP\authCode.txt")
+        Dim refreshTokenPathText As XmlText = XmlDoc.CreateTextNode("C:\TEMP\OneDriveRefreshToken.txt")
 
-        myXmlDocument.Save(XmlDocPath)
+        taskNode.AppendChild(authCodePathNode)
+        taskNode.AppendChild(refreshTokenPathNode)
+        authCodePathNode.AppendChild(authCodePathText)
+        refreshTokenPathNode.AppendChild(refreshTokenPathText)
     End Sub
-
 
 End Class
